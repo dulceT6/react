7e6cca0c2fae35ffe0b91065ddd1e0526ddf5f25
Valentino Parma <parmavalentinop@gmail.com>
2026-09-23 12:53:29 -0300

    les paul

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ function Hero (){
     const [variableButon, setVariableBoton]=useState(true)
 
     return(
-    <>
+       <>
 
     <div className="pepe">
     <h1>top 5 guitarras que me gustan</h1>
@@ -13,11 +13,12 @@ function Hero (){
     <h2>guitarras electricas</h2>
     </div>
     <p>en este top vamos a ver 5 mejores guitarras que me gustan,desde la que considero en mi opinion las mas linda esteticamente </p>
-    <h2>top 5-telecaster deluxe</h2>
-    <img src="./61u7rrwk3sL.jpg" alt="telecaster deluxe" width="100"/>
-    <p>me gusta esta guitarra por su forma y su estruc</p>
+    <h2>top 5-Telecaster deluxe</h2>
+    <img src="./61u7rrwk3sL.jpg" alt="telecaster deluxe" width="400"/>
+    <p>me gusta esta guitarra por su versatilidad sonora y su simpleza  </p>
+    <h2>top 4-Gibson Les Paul</h2>
+    <img  src="gibson-les-paul-standard-60s-bb-thm462510-w900" alt="Gibson Les Paul" width="400"/> 
     </>
-
     )
 }
  export default Hero
